ac30a4449ec3e64a32a3170bc602c8a99b22fd27
vladyrz <vlaruiz857@gmail.com>
2024-04-21 10:15:13 -0600

    App movil de easychat

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private browser: InAppBrowser) {}
+
+  ngOnInit(): void {
+    const browserInstance = this.browser.create(
+      "https://easychat.easyprocr.com/",
+      "_self", 
+      "location=no,hidenavigationbuttons=true,hideurlbar=true,zoom=no"
+    );
+    browserInstance.on("exit").subscribe(evt => {
+      App.exitApp();
+    })
+  }
 
 }
